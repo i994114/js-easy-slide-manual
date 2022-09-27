c0bf10d81f0a4688798f13b294269956c0cb981e
i994114 <i994114@hotmail.com>
2022-09-27 16:47:59 +0900

    modal実装

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
+
+
+
+$(function() {
+    //画面右端の「隠す」ボタンONでmodalを出す
+    $('.js-header-button').on('click', function() {
+        $('.js-header-button').hide();
+        $('.js-show-modal-cover').show();
+        $('.js-modal').show();
+    });
+
+    //modal内の「閉じる」ボタンonで現状に復帰する
+    $('.js-btn-close').on('click', function() {
+        console.log('faksjd');
+        $('.js-header-button').show();
+        $('.js-show-modal-cover').hide();
+        $('.js-modal').hide();
+    });
+
+    /*
+    $('.js-img-prev').on('click', function(e) {
+        e.preventDefault();
+
+        $.ajax({
+            type: 'post',
+            url: 'aaa.txt',
+            datatype: 'text',
+        }).done(function(data) {
+            console.log(data);
+            //$('.js-msg').html(data);
+        });
+    });
+*/
+});
+
+//---------
+//画像スライド
+//---------
 var slider = (function() {
     var currentImgNum = 1;  //現在選択されている画像
     var $imgParent = $('.img-parent');
@@ -26,10 +64,22 @@ var slider = (function() {
 
             $('.js-img-prev').on('click', function() {
                 that.imgPrev();
+/*
+                $.ajax({
+                    type: 'post',
+                    url: 'index.html',
+                }).done(function(data) {
+                    console.log('success');
+                    //$('.js-msg').html(data);
+                }).fail(function(data) {
+                    console.log('error');
+                });
+*/
             });
             $('.js-img-next').on('click', function() {
                 that.imgNext();
             });
+            
         }
     }
     
